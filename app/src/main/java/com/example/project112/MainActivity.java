4d9cab4bd933b2bbde307bf9492c61c2dbea5042
Mislav Franjo <mislav.franja@gmail.com>
2022-03-29 13:22:34 +0200

    added function for loading activitys on click

--- app/src/main/java/com/example/project112/MainActivity.java
+++ app/src/main/java/com/example/project112/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +14,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void EmergancyOnClick()
+    public void emergancyOnClick(View view)
     {
-        Intent intent = new Intent();
+        Intent intent = new Intent(this, Emergancy_Activity.class);
+        startActivity(intent);
+    }
 
+    public void locateOnClick(View view)
+    {
+        Intent intent = new Intent(this, Locate_Activity.class);
+        startActivity(intent);
+    }
+
+    public void personalDataOnClick(View view)
+    {
+        Intent intent = new Intent(this, UserData_Activity.class);
+        startActivity(intent);
     }
 }
